2afb89e756db4b73d24f92d737ccc6830a957322
monnbeom_kim <mbkim@cau.ac.kr>
2020-12-03 17:51:53 +0900

    1-9 중첩된 참조형 데이터(객체)의 프로퍼티 재할당

---
+++ homework1/1-9.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = a;
+var obj1 = { c: 10, d: 'ddd' };
+var obj2 = obj1;
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("obj1 = ", obj1);
+console.log("obj2.c, obj2.d = ", obj2.c, obj2.d);
+
+b = 15;
+obj2 = { c: 20, d: 'ddd' };
+
+console.log("b = ", b);
+console.log("obj2.c, obj2.d = ", obj2.c, obj2.d);
