9feff114cf8318d1fd87827a364a8983c0dc8b9c
monnbeom_kim <mbkim@cau.ac.kr>
2020-12-03 16:47:42 +0900

    1-3 데이터 할당 및 문자열 결합 예제

---
+++ homework1/1-3.js
@@ -0,0 +1,14 @@
+var a = 'abc';
+console.log("a = ", a);
+a = a + 'def';
+console.log("a = ", a);
+
+var b = 5;
+console.log("b = ", b);
+
+var c = 5;
+console.log("c = ", c);
+
+b = 7;
+console.log("b = ", b);
+
