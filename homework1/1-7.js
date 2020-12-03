d2719a5d400399b6a45adfb462f1864337367ec6
monnbeom_kim <mbkim@cau.ac.kr>
2020-12-03 17:47:31 +0900

    1-7 참조형 데이터의 프로퍼티 재할당

---
+++ homework1/1-7.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b = a;
+
+var obj1 = { c: 10, d: 'ddd' };
+var obj2 = obj1;
+
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("obj1 = ", obj1);
+console.log("obj2.c, obj2.d = ", obj2.c, obj2.d);
